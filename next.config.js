95f96ce36bf3164e62877d1acdc72d162120f7f0
Aaron Gooch <aaron@realio.fund>
2022-09-08 08:41:21 -0700

    turn off lint check on nextbuild

--- next.config.js
+++ next.config.js
@@ -9,4 +9,9 @@ module.exports = nextTranslate({
     });
     return config;
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 });
