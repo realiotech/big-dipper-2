d74fcdeb5ed6882d6a853c4f188f66b6b670c3f2
Aaron Gooch <aaron@realio.fund>
2022-09-12 10:03:51 -0700

    log what is in the dist folder

--- bin/build_next_js.sh
+++ bin/build_next_js.sh
@@ -2,8 +2,8 @@
 
 NODE_ENV=production npm run build
 
-ls -lha /app
-
 ls -lha /app/.next
+
+ls -lha /app/dist
 
 echo "Next.js build ready"
--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
 ls -lha /app/.next
+ls -lha /app/dist
 
 exec node /app/dist/index.js
