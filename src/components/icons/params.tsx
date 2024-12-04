005c100b1e136487b5ff52c42109fa017eef4198
Dzung Do <neitdung@gmail.com>
2024-12-04 07:43:33 +0700

    remove some warnings

--- src/components/icons/params.tsx
+++ src/components/icons/params.tsx
@@ -4,12 +4,12 @@ export const Params = () => {
     return (
         <Icon>
             <svg width="22" height="16" viewBox="0 0 22 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M2.875 3.625C3.56536 3.625 4.125 3.06536 4.125 2.375C4.125 1.68464 3.56536 1.125 2.875 1.125C2.18464 1.125 1.625 1.68464 1.625 2.375C1.625 3.06536 2.18464 3.625 2.875 3.625Z" stroke="currentColor" stroke-width="1.5" />
-                <path d="M6.625 1.125H20.375M6.625 3.625H14.125" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
-                <path d="M2.875 9.25C3.56536 9.25 4.125 8.69036 4.125 8C4.125 7.30964 3.56536 6.75 2.875 6.75C2.18464 6.75 1.625 7.30964 1.625 8C1.625 8.69036 2.18464 9.25 2.875 9.25Z" stroke="currentColor" stroke-width="1.5" />
-                <path d="M6.625 6.75H16.625M6.625 9.25H15.375" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
-                <path d="M2.875 14.875C3.56536 14.875 4.125 14.3154 4.125 13.625C4.125 12.9346 3.56536 12.375 2.875 12.375C2.18464 12.375 1.625 12.9346 1.625 13.625C1.625 14.3154 2.18464 14.875 2.875 14.875Z" stroke="currentColor" stroke-width="1.5" />
-                <path d="M6.625 12.375H18.5M6.625 14.875H11.625" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+                <path d="M2.875 3.625C3.56536 3.625 4.125 3.06536 4.125 2.375C4.125 1.68464 3.56536 1.125 2.875 1.125C2.18464 1.125 1.625 1.68464 1.625 2.375C1.625 3.06536 2.18464 3.625 2.875 3.625Z" stroke="currentColor" strokeWidth="1.5" />
+                <path d="M6.625 1.125H20.375M6.625 3.625H14.125" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+                <path d="M2.875 9.25C3.56536 9.25 4.125 8.69036 4.125 8C4.125 7.30964 3.56536 6.75 2.875 6.75C2.18464 6.75 1.625 7.30964 1.625 8C1.625 8.69036 2.18464 9.25 2.875 9.25Z" stroke="currentColor" strokeWidth="1.5" />
+                <path d="M6.625 6.75H16.625M6.625 9.25H15.375" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+                <path d="M2.875 14.875C3.56536 14.875 4.125 14.3154 4.125 13.625C4.125 12.9346 3.56536 12.375 2.875 12.375C2.18464 12.375 1.625 12.9346 1.625 13.625C1.625 14.3154 2.18464 14.875 2.875 14.875Z" stroke="currentColor" strokeWidth="1.5" />
+                <path d="M6.625 12.375H18.5M6.625 14.875H11.625" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
             </svg>
         </Icon>
     )
