2617e30a78f9f21f4f45e5a54191c1a9a5e470af
Du Nguyen <dunguyen643@gmail.com>
2025-02-07 14:40:33 +0700

    Fix bug SSR and update UI

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,63 +1,190 @@
-import { VStack, Image, SimpleGrid, GridItem, Text, Link, HStack } from "@chakra-ui/react";
+import {
+  VStack,
+  Image,
+  SimpleGrid,
+  GridItem,
+  Text,
+  Link,
+  HStack,
+} from "@chakra-ui/react";
 import { LinkedIn } from "../icons/linkedin";
 import { Telegram } from "../icons/telegram";
 import { Twitter } from "../icons/twitter";
 import { Github } from "../icons/github";
 import { useColorMode } from "../ui/color-mode";
 import { FaDiscord, FaDochub } from "react-icons/fa";
+import { useState, useEffect } from "react";
 export default function Footer() {
-    const { colorMode } = useColorMode()
+  const { colorMode } = useColorMode();
+  const [logoSrc, setLogoSrc] = useState("/images/logo-full.svg");
+  useEffect(() => {
+    setLogoSrc(
+      colorMode === "dark"
+        ? "/images/logo-full-white.svg"
+        : "/images/logo-full.svg"
+    );
+  }, [colorMode]);
+  return (
+    <SimpleGrid
+      w="full"
+      columns={{ base: 1, lg: 6 }}
+      my="10"
+      px="10"
+      py="10"
+      divideY={{ base: "2px", lg: "0px" }}
+      boxShadow={{
+        base: "0px 0px 10px 2px #0000001A",
+        lg: "0px 0px 10px 2px #0000001A",
+      }}
+      borderRadius={"20px"}
+      bg={{ base: "white", _dark: "#0F0F0F" }}
+    >
+      <GridItem colSpan={{ base: 1, lg: 3 }} pb="15px">
+        <VStack align="left" gap="20px">
+          <Image
+            key={logoSrc}
+            h="50px"
+            fit="contain"
+            w="min-content"
+            src={logoSrc}
+            alt="Logo"
+          />
 
-    return (
-        <SimpleGrid 
-            w='full'
-            columns={{ base: 1, lg: 6 }}
-            my='10'
-            px='10'
-            py='10'
-            divideY={{ base: '2px', lg: '0px'}}
-            boxShadow={{base: '0px 0px 10px 2px #0000001A' , lg: '0px 0px 10px 2px #0000001A'}}
-            borderRadius={'20px'}
-            bg={{base: 'white', _dark: '#0F0F0F'}}
+          <Text>Realio Network Block Explorer</Text>
+        </VStack>
+      </GridItem>
+      <GridItem py={{ base: "15px", lg: "0px" }}>
+        <VStack
+          color={{ base: "#707D8A", _dark: "#9DA7B0" }}
+          align="left"
+          gap="20px"
+        >
+          <Text
+            color={{ base: "#707D8A", _dark: "#9DA7B0" }}
+            fontSize="16px"
+            fontWeight={600}
+          >
+            Realio Tokens
+          </Text>
+          <Link
+            color={{ base: "black", _dark: "white" }}
+            href="#"
+            _hover={{ color: "#173DA6" }}
+            fontWeight={400}
+            fontSize={"14px"}
+          >
+            Liquid Mining Fund
+          </Link>
+          <Link
+            href="#"
+            _hover={{ color: "#173DA6" }}
+            fontWeight={400}
+            color={{ base: "black", _dark: "white" }}
+            fontSize={"14px"}
+          >
+            Realio Network Token
+          </Link>
+          <Link
+            href="#"
+            _hover={{ color: "#173DA6" }}
+            fontWeight={400}
+            color={{ base: "black", _dark: "white" }}
+            fontSize={"14px"}
+          >
+            Realio Network, LTD
+          </Link>
+        </VStack>
+      </GridItem>
+      <GridItem py={{ base: "15px", lg: "0px" }}>
+        <VStack align="left" gap="20px">
+          <Text
+            color={{ base: "#707D8A", _dark: "#9DA7B0" }}
+            fontSize="16px"
+            fontWeight={600}
+          >
+            Realio Websites
+          </Text>
+          <Link
+            href="https://realio.network"
+            _hover={{ color: "#173DA6" }}
+            fontWeight={400}
+            color={{ base: "black", _dark: "white" }}
+            fontSize={"14px"}
+          >
+            Realio Network
+          </Link>
+          <Link
+            href="https://realio.fund/"
+            _hover={{ color: "#173DA6" }}
+            fontWeight={400}
+            color={{ base: "black", _dark: "white" }}
+            fontSize={"14px"}
+          >
+            Realio Fund
+          </Link>
+          <Link
+            href="https://liquidmining.fund/"
+            _hover={{ color: "#173DA6" }}
+            fontWeight={400}
+            color={{ base: "black", _dark: "white" }}
+            fontSize={"14px"}
+          >
+            Liquid Mining Fund
+          </Link>
+          <Link
+            href="https://freehold.finance/"
+            _hover={{ color: "#173DA6" }}
+            fontWeight={400}
+            color={{ base: "black", _dark: "white" }}
+            fontSize={"14px"}
+          >
+            Freehold
+          </Link>
+          <Link
+            href="https://districts.xyz/"
+            _hover={{ color: "#173DA6" }}
+            fontWeight={400}
+            color={{ base: "black", _dark: "white" }}
+            fontSize={"14px"}
+          >
+            Districts
+          </Link>
+        </VStack>
+      </GridItem>
+      <GridItem py={{ base: "15px", lg: "0px" }}>
+        <VStack align="left" gap="20px">
+          <Text
+            color={{ base: "#707D8A", _dark: "#9DA7B0" }}
+            fontSize="16px"
+            fontWeight={600}
+          >
+            Community
+          </Text>
+          <HStack gap="4" color={{ base: "white", _dark: "black" }}>
+            <Link
+              href="https://www.linkedin.com/company/realio"
+              fontSize={"20px"}
             >
-            <GridItem colSpan={{ base: 1, lg: 3 }} pb='15px'>
-                <VStack align='left' gap='20px'>
-                    {colorMode == 'light' ? <Image  src='/images/logo-full.svg' h='50px' fit='contain' w='min-content' /> : <Image  src='/images/logo-full-white.svg' h='50px' fit='contain' w='min-content' />}
-                    <Text>
-                        Realio Network Block Explorer
-                    </Text>
-                </VStack>
-            </GridItem>
-            <GridItem py={{ base: '15px', lg: '0px' }}>
-                <VStack color={{base: "#707D8A", _dark: "#9DA7B0"}} align='left' gap='20px'>
-                    <Text color={{base: "#707D8A", _dark: "#9DA7B0"}} fontSize='16px' fontWeight={600}>Realio Tokens</Text>
-                    <Link color={{base: 'black', _dark: 'white'}} href="#" _hover={{ color: "#173DA6" }} fontWeight={400} fontSize={'14px'}>Liquid Mining Fund</Link>
-                    <Link href="#" _hover={{ color: "#173DA6" }} fontWeight={400} color={{base: 'black', _dark: 'white'}} fontSize={'14px'}>Realio Network Token</Link>
-                    <Link href="#" _hover={{ color: "#173DA6" }} fontWeight={400} color={{base: 'black', _dark: 'white'}} fontSize={'14px'}>Realio Network, LTD</Link>
-                </VStack>
-            </GridItem>
-            <GridItem py={{ base: '15px', lg: '0px' }}>
-                <VStack align='left' gap='20px'>
-                    <Text color={{base: "#707D8A", _dark: "#9DA7B0"}} fontSize='16px' fontWeight={600}>Realio Websites</Text>
-                    <Link href="https://realio.network" _hover={{ color: "#173DA6" }} fontWeight={400} color={{base: 'black', _dark: 'white'}} fontSize={'14px'}>Realio Network</Link>
-                    <Link href="https://realio.fund/" _hover={{ color: "#173DA6" }} fontWeight={400} color={{base: 'black', _dark: 'white'}} fontSize={'14px'}>Realio Fund</Link>
-                    <Link href="https://liquidmining.fund/" _hover={{ color: "#173DA6" }} fontWeight={400} color={{base: 'black', _dark: 'white'}} fontSize={'14px'}>Liquid Mining Fund</Link>
-                    <Link href="https://freehold.finance/" _hover={{ color: "#173DA6" }} fontWeight={400} color={{base: 'black', _dark: 'white'}} fontSize={'14px'}>Freehold</Link>
-                    <Link href="https://districts.xyz/" _hover={{ color: "#173DA6" }} fontWeight={400} color={{base: 'black', _dark: 'white'}} fontSize={'14px'}>Districts</Link>
-                </VStack>
-            </GridItem>
-            <GridItem py={{ base: '15px', lg: '0px' }}>
-                <VStack align='left' gap='20px'>
-                    <Text color={{base: "#707D8A", _dark: "#9DA7B0"}} fontSize='16px' fontWeight={600}>Community</Text>
-                    <HStack  gap='4' color={{base: "white", _dark: "black"}} >
-                        <Link href="https://www.linkedin.com/company/realio" fontSize={'20px'}><LinkedIn /></Link>
-                        <Link href="https://discord.com/invite/WhRgHEfDF4" fontSize={'20px'}><FaDiscord /></Link>
-                        <Link href="https://x.com/realio_network" fontSize={'20px'}><Twitter /></Link>
-                        <Link href="https://github.com/realiotech" fontSize={'20px'}><Github /></Link>
-                        <Link href="https://docs.realio.network/" fontSize={'20px'}><FaDochub /></Link>
-                    </HStack>
-                </VStack>
-            </GridItem>
-        </SimpleGrid>
-    )
+              <LinkedIn />
+            </Link>
+            <Link
+              href="https://discord.com/invite/WhRgHEfDF4"
+              fontSize={"20px"}
+            >
+              <FaDiscord />
+            </Link>
+            <Link href="https://x.com/realio_network" fontSize={"20px"}>
+              <Twitter />
+            </Link>
+            <Link href="https://github.com/realiotech" fontSize={"20px"}>
+              <Github />
+            </Link>
+            <Link href="https://docs.realio.network/" fontSize={"20px"}>
+              <FaDochub />
+            </Link>
+          </HStack>
+        </VStack>
+      </GridItem>
+    </SimpleGrid>
+  );
 }
--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,5 +1,6 @@
+import { useState, useEffect } from "react";
 import { Center, Image, Link, VStack } from "@chakra-ui/react";
-import NextLink from "next/link"
+import NextLink from "next/link";
 import NavLink from "./navlink";
 import { Home } from "../icons/home";
 import { Block } from "../icons/block";
@@ -7,30 +8,63 @@ import { Stake } from "../icons/stake";
 import { Transaction } from "../icons/transaction";
 import { Proposal } from "../icons/proposal";
 import { useRouter } from "next/router";
-import { ColorModeButton } from "../ui/color-mode";
 import { useColorMode } from "../ui/color-mode";
+import { Switch } from "@/components/ui/switch";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 export default function Sidebar() {
-    const { pathname } = useRouter()
-    const { colorMode } = useColorMode()
+  const { pathname } = useRouter();
+  const { colorMode, toggleColorMode } = useColorMode();
 
-    return (
-        <VStack hideBelow='lg' pr='50px'>
-            <Link asChild outline='none'>
-                <NextLink href="/">
-                {colorMode == 'light' ? <Image w='60px' src="/images/logo.svg" /> : <Image w='60px' src="/images/logo_white.svg" />}
-                </NextLink>
-            </Link>
-            <Center position='relative' top='200px'>
-                <VStack gap='20px'>
-                    <NavLink href="/" selected={pathname == "/"} children={<Home />} />
-                    <NavLink href="/blocks" selected={pathname.includes("blocks")} children={<Block />} />
-                    <NavLink href="/validators" selected={pathname.includes("validators")} children={<Stake />} />
-                    <NavLink href="/transactions" selected={pathname.includes("transactions")} children={<Transaction />} />
-                    <NavLink href="/proposals" selected={pathname.includes("proposals")} children={<Proposal />} />
-                    <ColorModeButton />
-                </VStack>
-            </Center>
+  // Default logo for SSR (avoids hydration mismatch)
+  const [logoSrc, setLogoSrc] = useState("/images/logo.svg");
+
+  useEffect(() => {
+    setLogoSrc(
+      colorMode === "dark" ? "/images/logo_white.svg" : "/images/logo.svg"
+    );
+  }, [colorMode]);
+
+  return (
+    <VStack hideBelow="lg" pr="50px">
+      <Link asChild outline="none">
+        <NextLink href="/">
+          <Image key={logoSrc} w="60px" src={logoSrc} alt="Logo" />
+        </NextLink>
+      </Link>
+      <Center position="relative" top="200px">
+        <VStack gap="20px">
+          <NavLink href="/" selected={pathname == "/"} children={<Home />} />
+          <NavLink
+            href="/blocks"
+            selected={pathname.includes("blocks")}
+            children={<Block />}
+          />
+          <NavLink
+            href="/validators"
+            selected={pathname.includes("validators")}
+            children={<Stake />}
+          />
+          <NavLink
+            href="/transactions"
+            selected={pathname.includes("transactions")}
+            children={<Transaction />}
+          />
+          <NavLink
+            href="/proposals"
+            selected={pathname.includes("proposals")}
+            children={<Proposal />}
+          />
+          <Switch
+            theme="light"
+            colorPalette="gray"
+            checked={colorMode === "dark"}
+            onCheckedChange={() => toggleColorMode()}
+            size="lg"
+            thumbLabel={{ on: <MdDarkMode />, off: <MdLightMode /> }}
+          />
         </VStack>
-    )
+      </Center>
+    </VStack>
+  );
 }
