f841e3388a00a9971be91b6df2237a3f5dcb1485
Hieu Vu <hieuvubk@gmail.com>
2025-09-03 17:18:02 +0700

    build delegate evm tx

--- src/utils/delegate_evm_transaction/index.tsx
+++ src/utils/delegate_evm_transaction/index.tsx
@@ -4,10 +4,57 @@ import { Any } from "cosmjs-types/google/protobuf/any";
 import {
   makeAuthInfoBytes, makeSignDoc,
 } from "@cosmjs/proto-signing";
-import { TxRaw } from "cosmjs-types/cosmos/tx/v1beta1/tx";
+import { TxRaw, TxBody } from "cosmjs-types/cosmos/tx/v1beta1/tx";
 import { Int53 } from "@cosmjs/math";
 import { fromBase64 } from "@cosmjs/encoding";
 import Big from "big.js";
+
+// Define the MsgDelegateEVM proto message structure
+const MsgDelegateEVM = {
+  encode(message: any): Uint8Array {
+    // Manual protobuf encoding for MsgDelegateEVM
+    const writer = new Uint8Array(1024); // Allocate buffer
+    let offset = 0;
+
+    // Field 1: delegator_address (string)
+    if (message.delegatorAddress) {
+      const delegatorBytes = new TextEncoder().encode(message.delegatorAddress);
+      writer[offset++] = 0x0a; // field 1, wire type 2 (length-delimited)
+      writer[offset++] = delegatorBytes.length;
+      writer.set(delegatorBytes, offset);
+      offset += delegatorBytes.length;
+    }
+
+    // Field 2: validator_address (string)
+    if (message.validatorAddress) {
+      const validatorBytes = new TextEncoder().encode(message.validatorAddress);
+      writer[offset++] = 0x12; // field 2, wire type 2 (length-delimited)
+      writer[offset++] = validatorBytes.length;
+      writer.set(validatorBytes, offset);
+      offset += validatorBytes.length;
+    }
+
+    // Field 3: contract_address (string)
+    if (message.contractAddress) {
+      const contractBytes = new TextEncoder().encode(message.contractAddress);
+      writer[offset++] = 0x1a; // field 3, wire type 2 (length-delimited)
+      writer[offset++] = contractBytes.length;
+      writer.set(contractBytes, offset);
+      offset += contractBytes.length;
+    }
+
+    // Field 4: amount (string)
+    if (message.amount) {
+      const amountBytes = new TextEncoder().encode(message.amount);
+      writer[offset++] = 0x22; // field 4, wire type 2 (length-delimited)
+      writer[offset++] = amountBytes.length;
+      writer.set(amountBytes, offset);
+      offset += amountBytes.length;
+    }
+
+    return writer.slice(0, offset);
+  }
+};
 
 // EVM Delegate message type for DSTRX tokens
 export const createDelegateEVMTx = async ({
@@ -48,21 +95,26 @@ export const createDelegateEVMTx = async ({
 
       // Create EVM delegate message for ERC20 tokens
       const delegateEVMMsg = {
-        typeUrl: "/realionetwork.multistaking.v1.MsgDelegateEVM",
-        value: {
-          delegatorAddress: sender,
-          validatorAddress: validator,
-          contractAddress: contractAddress,
-          amount: Big(amount).times(Big(10).pow(decimal)).toFixed(0),
-        },
+        delegatorAddress: sender,
+        validatorAddress: validator,
+        contractAddress: contractAddress,
+        amount: Big(amount).times(Big(10).pow(decimal)).toFixed(0),
       };
+
+      console.log('=== Custom Encoding Debug ===');
+      console.log('delegateEVMMsg:', delegateEVMMsg);
 
       const fee = {
         amount: coins(fees, "ario"), // Use RIO for fees
         gas: gas,
       };
 
+      console.log('=== Fee Debug ===', fee);
+
       const accountResponse = await fetch(`${apiEndpoint}/cosmos/auth/v1beta1/accounts/${sender}`);
+
+      console.log('Account response:', accountResponse);
+      console.log('path', `${apiEndpoint}/cosmos/auth/v1beta1/accounts/${sender}`);
 
       if (!accountResponse.ok) {
         throw new Error(`Failed to fetch account data: ${accountResponse.status} ${accountResponse.statusText}`);
@@ -82,17 +134,35 @@ export const createDelegateEVMTx = async ({
         value: PubKey.encode({ key: account.pubkey }).finish(),
       });
 
-      const txBody = {
-        typeUrl: "/cosmos.tx.v1beta1.TxBody",
-        value: { messages: [delegateEVMMsg], memo },
+      // Encode the MsgDelegateEVM using our custom encoder
+      const msgBytes = MsgDelegateEVM.encode(delegateEVMMsg);
+      console.log('Encoded message bytes:', msgBytes);
+
+      // Create the Any wrapper for the message
+      const anyMsg = Any.fromPartial({
+        typeUrl: "/multistaking.v1.MsgDelegateEVM",
+        value: msgBytes,
+      });
+
+      // Create TxBody manually
+      const txBodyValue = {
+        messages: [anyMsg],
+        memo: memo || "",
+        timeoutHeight: BigInt(0),
+        extensionOptions: [],
+        nonCriticalExtensionOptions: [],
       };
 
-      const txBodyBytes = signer.registry.encode(txBody);
+      const txBodyBytes = TxBody.encode(txBodyValue).finish();
+      console.log('TxBody bytes:', txBodyBytes);
+
       const gasLimit = Int53.fromString(fee.gas).toNumber();
       const authInfoBytes = makeAuthInfoBytes(
         [{ pubkey: pubKey, sequence }],
         fee.amount,
         gasLimit,
+        undefined,
+        undefined
       );
       const signDoc = makeSignDoc(txBodyBytes, authInfoBytes, chainId, accountNumber);
 
