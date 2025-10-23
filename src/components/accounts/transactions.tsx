aa980749f9191318f172b082b6ed04ccdda0aa30
Nguyen Du <nguyendu@Nguyens-MacBook-Pro.local>
2025-10-23 16:40:33 +0700

    feat: implement pagination for transactions with dynamic page handling

--- src/components/accounts/transactions.tsx
+++ src/components/accounts/transactions.tsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, Center } from "@chakra-ui/react";
 import { useTransactions } from "./hooks";
 import TxTable from "@/components/transactions/table";
+import Pagination from "@/components/layout/pagination";
 
 export default function Transactions() {
-  const { state } = useTransactions();
+  const { state, pageInfo, handlePageChange } = useTransactions();
 
   return (
     <Box bg={{ base: "#FAFBFC", _dark: "#0F0F0F" }} overflow={"auto"} py={6} px={2} borderRadius="md" boxShadow="sm" mb={8}>
       <Text fontSize="lg" px={4} fontWeight="bold" mb="4">
         Transactions
       </Text>
-      <TxTable transactions={state.data} isLoading={false} />
+      <TxTable transactions={state.data} isLoading={state.isNextPageLoading} />
+
+      {state.data.length > 0 && (
+        <Center w="full" py="4">
+          <Pagination
+            pageInfo={pageInfo}
+            pageChangeFunc={handlePageChange}
+            pageSizeChangeFunc={() => {}}
+          />
+        </Center>
+      )}
     </Box>
   );
 }
